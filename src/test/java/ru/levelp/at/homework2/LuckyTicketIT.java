fb8a5bc63f145cf460e7be64b144a101497400e8
ShvargonovaArina <arina.shvargonova@yandex.ru>
2023-02-22 22:58:20 +0700

    add tests

---
+++ src/test/java/ru/levelp/at/homework2/LuckyTicketIT.java
@@ -0,0 +1,40 @@
+package ru.levelp.at.homework2;
+
+import static ru.levelp.at.homework2.LuckyTicketCheck.ticketIsLucky;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class LuckyTicketIT {
+
+    @Test
+    @Tag (TagNames.POSITIVE_TAG_NAME)
+    void ticketSumFirst3DigitsEqualSumSecond3Digits() {
+        String ticketNumber = "123123";
+        Assertions.assertTrue(ticketIsLucky(ticketNumber));
+    }
+
+    @Test
+    @Tag (TagNames.NEGATIVE_TAG_NAME)
+    void ticketSumFirst3DigitsNotEqualSumSecond3Digits() {
+        String ticketNumber = "123456";
+        Assertions.assertFalse(ticketIsLucky(ticketNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("ru.levelp.at.homework2.LuckyTicketDataProvider#lengthLessThanSixDataProvider")
+    @Tag (TagNames.NEGATIVE_TAG_NAME)
+    void ticketLengthLessThan6(String ticketNumber) {
+        Assertions.assertFalse(ticketIsLucky(ticketNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("ru.levelp.at.homework2.LuckyTicketDataProvider#lengthMoreThanSixDataProvider")
+    @Tag (TagNames.NEGATIVE_TAG_NAME)
+    void ticketLengthMoreThan6(String ticketNumber) {
+        Assertions.assertFalse(ticketIsLucky(ticketNumber));
+    }
+}
